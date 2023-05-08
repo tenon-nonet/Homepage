60d97a1e9141a606abe7210b6f8bcacf175499fc
tenon-nonet <nonet0421@gmail.com>
2023-05-08 19:59:31 +0900

    feat:center_textのtypicalの変更

--- src/components/home/center_text.jsx
+++ src/components/home/center_text.jsx
@@ -6,13 +6,18 @@ const Center_Text = () => {
       <Typical
         steps={[
           "Hello!", 2000, "H", 1000,
-          "HOME Page", 4000, "H", 2000,
-          "Haste makes waste.", 5000, "H", 3000,
-          "Health is better than wealth.", 5500, "H", 3500,
-          "History repeats itself.", 6000, "H", 4000,
-          "Hunger is the best sauce.", 5000, "H", 3000,
-          "Honesty is the best policy.",6000,"H", 3000,
-          "Hahahahahahahahahahahahahahahahahahahahahahahahahahahahah!!!",
+          "HOME Page", 3000, "H", 2000,
+          "Haste makes waste.", 2000, "H", 1000,
+          "Health is better than wealth.", 2000, "H", 1000,
+          "History repeats itself.", 2000, "H", 1200,
+          "Hunger is the best sauce.", 2000, "H", 1200,
+          "Honesty is the best policy.",2000,"H", 1200,
+          "He looks tired.", 3000,"He", 3000,"H",3000,
+          "",10000,
+          "W",3000,"WELL...",4000,"WELCAM",300,"WELCOME Page",10000,
+          "",5000,"...",3000,"...Hi",3000,
+          "Hello...", 3000, "H", 3000,
+          "HOME",2000,"HOME Page!"
         ]}
         wrapper="h2"
       />
