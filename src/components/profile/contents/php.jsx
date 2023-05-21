836839f8b8240673ef49ad0c795559a80b7e8763
tenon-nonet <nonet0421@gmail.com>
2023-05-22 02:03:24 +0900

    add:profileのphp内の文章を作成

--- src/components/profile/contents/php.jsx
+++ src/components/profile/contents/php.jsx
@@ -4,17 +4,28 @@ const php = () => {
     return (
         <>
             <Typography variant="h5">・PHP</Typography><br />
+            <Typography variant="p">
+                2つ目に学習を始めた言語。<br />
+                本管理アプリを作成したが、基礎が習得できておらず、<br />
+                基礎から学習することの重要性を知る機会となった。<br /><br />
+                読み上げアプリ、掲示板、本管理アプリを作成。<br /><br />
+                また、読み上げアプリの作成にて、<br />
+                初めてGitを活用し、共同で開発を行なった。<br /><br />
+                progate、paiza、Udemy、教本、<br />
+                経験者から指導していただき学習を行った。<br /><br />
+            </Typography>
+            <hr />
             <Typography variant="h5">・Laravel</Typography><br />
             <Typography variant="p">
                 初めて使用したフレームワーク<br />
-                本管理アプリを作成したが、PHPの基礎が習得できておらず、<br />
-                基礎から学習することの重要性を知る機会となった。
+                本管理アプリを作成した。<br /><br />
+                以下は、学習に使用した教材となる。<br /><br />
             </Typography>
             <Typography variant="p">
                 <a href="https://www.hypertextcandy.com/laravel-tutorial-introduction">
-                    動かして学ぶ！Laravel開発入門
+                    動かして学ぶ！Laravel開発入門　～初心者が作りながら学ぶWebアプリ開発の基本～
                 </a><br />
-                山崎大助 著
+                山崎大助 著<br /><br />
             </Typography>
             <Typography variant="p">
                 <a href="https://www.hypertextcandy.com/laravel-tutorial-introduction">
